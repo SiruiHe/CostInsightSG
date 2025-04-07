d8aee546d668f1ec383591bb097566ebece7c279
itsjeremyhsieh <jeremy.study0107@gmail.com>
2025-04-07 23:54:55 +0800

    Fix: edit term

--- src/partials/dashboard/DashboardBarChart.vue
+++ src/partials/dashboard/DashboardBarChart.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col col-span-full sm:col-span-6  bg-white dark:bg-gray-800 shadow-xs rounded-xl">
     <header class="px-5 py-4 border-b border-gray-100 dark:border-gray-700/60">
       <div class="flex">
-        <h2 class="font-semibold text-gray-800 dark:text-gray-100">CPI VS Individual income</h2>
+        <h2 class="font-semibold text-gray-800 dark:text-gray-100">CPI vs. Individual income</h2>
         <HelpTip :content="tipContent" :width="9"></HelpTip>
       </div>
     </header>
